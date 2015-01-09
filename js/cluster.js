4316ab198661182728ed27aa4455833dc92668b9
Assaf Akrabi <akrabi@mail.com>
2015-01-09 23:57:57 +0200

    - Move modules to 'modules' folder - Unite entire 3 step process under index.html page.

---
+++ js/cluster.js
@@ -0,0 +1,154 @@
+
+function parseData(data) {
+    var labels = new Array();
+    var vectors = new Array();
+    lines = data.split("\n");
+    for (var i = 0; i < lines.length; i++) {
+        if (lines[i].length == 0)
+            continue;
+        var elements = lines[i].split(",");
+        var label = elements.shift();
+        var vector = new Array();
+        for (j = 0; j < elements.length; j++)
+            vector.push(parseFloat(elements[j]));
+        vectors.push(vector);
+        labels.push(label);
+    }
+    return {'labels': labels, 'vectors': vectors};
+}
+
+function runAlgo() {
+    var domobj = document.getElementById('algo');
+    var algo = domobj.options[domobj.selectedIndex].value;
+    switch (algo) {
+        case 'kmeans':
+        {
+            runKM();
+            break;
+        }
+        case 'hierarchical':
+        {
+            runHC();
+            break;
+        }
+        case 'fcmeans':
+        {
+            runFCM();
+            break;
+        }
+    }
+    document.getElementById('output_panel').style.display = 'block';
+}
+
+function showParamPanel(visiblePanel) {
+    var panelNames = ['km_panel', 'fcm_panel', 'hc_panel'];
+    for (var i = 0; i < panelNames.length; i++)
+        document.getElementById(panelNames[i]).style.display = 'none';
+    document.getElementById(visiblePanel).style.display = 'block';
+}
+
+function updateAlgo() {
+    var domobj = document.getElementById('algo');
+    var algo = domobj.options[domobj.selectedIndex].value;
+    switch (algo) {
+        case 'kmeans':
+        {
+            showParamPanel('km_panel');
+            break;
+        }
+        case 'hierarchical':
+        {
+            showParamPanel('hc_panel');
+            break;
+        }
+        case 'fcmeans':
+        {
+            showParamPanel('fcm_panel');
+            break;
+        }
+    }
+    document.getElementById('text').innerHTML = "";
+    document.getElementById('output_panel').style.display = 'none';
+}
+
+function runKM() {
+    var data = parseData(document.getElementById('data').value);
+    var vectors = data['vectors'];
+    var labels = data['labels'];
+    var domobj = document.getElementById('KM-K');
+    var K = parseInt(domobj.options[domobj.selectedIndex].value);
+    var clusters = figue.kmeans(K, vectors);
+
+    var txt;
+    if (clusters) {
+        txt = "<table border='1'>";
+        txt += "<tr><th>Label</th><th>Vector</th><th>Cluster id</th><th>Cluster centroid</th></tr>";
+
+        for (var i = 0; i < vectors.length; i++) {
+            var index = clusters.assignments[i];
+            txt += "<tr><td>" + labels[i] + "</td><td>" + vectors[i] + "</td><td>" + index + "</td><td>" + clusters.centroids[index] + "</td></tr>";
+        }
+        txt += "</table>"
+    } else
+        txt = "No result (too many clusters/too few different instances (try changing K)";
+    document.getElementById('text').innerHTML = txt;
+}
+
+function runHC() {
+    var data = parseData(document.getElementById('data').value);
+    var domobj = document.getElementById('space');
+    var space = parseInt(domobj.options[domobj.selectedIndex].value);
+    var balanced = (radioValue('balanced') === 'true');
+    var withLabel = (radioValue('withLabel') === 'true');
+    var withCentroid = (radioValue('withCentroid') === 'true');
+    var withDistance = (radioValue('withDistance') === 'true');
+    var linkage = parseInt(radioValue('linkage'));
+    var distance = parseInt(radioValue('distance'));
+    root = figue.agglomerate(data['labels'], data['vectors'], distance, linkage);
+    var pre = document.getElementById('text');
+    var text = root.buildDendogram(space, balanced, withLabel, withCentroid, withDistance);
+    if (document.all) {
+        pre.innerText = text;
+    } else {
+        pre.innerHTML = text;
+    }
+}
+
+function runFCM() {
+    var data = parseData(document.getElementById('data').value);
+    var vectors = data['vectors'];
+    var labels = data['labels'];
+    var domobj = document.getElementById('FCM-K');
+    var K = parseInt(domobj.options[domobj.selectedIndex].value);
+    var fuzziness = parseInt(document.getElementById('fuzziness').value);
+    var epsilon = parseInt(document.getElementById('epsilon').value);
+
+    var clusters = figue.fcmeans(K, vectors, epsilon, fuzziness);
+
+    var txt;
+    if (clusters) {
+        txt = "<table border='1'>";
+        txt += "<tr><th>Cluster id</th><th>Cluster centroid</th></tr>";
+
+        for (var i = 0; i < K; i++) {
+            txt += "<tr><td>" + i + "</td><td>" + clusters.centroids[i] + "</td></tr>";
+        }
+        txt += "</table>"
+        txt += "<br/>"
+        txt += clusters.membershipMatrix;
+
+    } else
+        txt = "No result (too many clusters/too few different instances (try changing K)";
+    document.getElementById('text').innerHTML = txt;
+}
+
+function radioValue(name) {
+    var radios = document.getElementsByName(name);
+    for (var i = 0; i < radios.length; i++)
+        if (radios[i].checked)
+            return radios[i].value;
+}
+
+window.onload = function () {
+    updateAlgo();
+}
--- js/main.js
+++ js/main.js
@@ -1,10 +1,51 @@
+function moveTo(step) {
+    var filter = $('#filter');
+    var cluster = $('#cluster');
+    var results = $('#results');
+    var filterStep = $('#filterStep');
+    var clusterStep = $('#clusterStep');
+    var resultsStep = $('#resultsStep');
+
+    if (step === 'filter') {
+        filter.show()
+        cluster.hide();
+        results.hide();
+        filterStep.removeClass('complete , disabled').addClass('active');
+        clusterStep.removeClass('complete , active').addClass('disabled');
+        resultsStep.removeClass('complete , active').addClass('disabled');
+    }
+    else if (step === 'cluster') {
+        filter.hide()
+        cluster.show();
+        results.hide();
+        filterStep.removeClass('active , disabled').addClass('complete');
+        clusterStep.removeClass('complete , disabled').addClass('active');
+        resultsStep.removeClass('complete , active').addClass('disabled');
+    }
+    else if (step === 'results') {
+        filter.hide()
+        cluster.hide();
+        results.show();
+        filterStep.removeClass('active , disabled').addClass('complete');
+        clusterStep.removeClass('active , disabled').addClass('complete');
+        resultsStep.removeClass('complete , disabled').addClass('active');
+    }
+    $( window ).scrollTop( 0 );
+}
+
 $( document ).ready(function() {
     // Initialize the map with the drawing controls
-    var map = new Map($('#map-canvas')[0]);
-    map.setDrawingManager($('#drawRectangle'), $('#drawCircle'), $('#drawPolygon'), $('#stopDrawing'), $('#deleteAllShapes'));
+    var map = new Map($('#map-canvas').get(0));
+    //map.setDrawingManager($('#drawRectangle'), $('#drawCircle'), $('#drawPolygon'), $('#stopDrawing'), $('#deleteAllShapes'));
     map.init();
+    map.hide();
 
     // Initialize slider
-    var slider = new TimeSlider($('#slider')[0]);
+    var slider = new TimeSlider($('#time_slider')[0]);
     slider.init();
+});
+
+$('#filterForm').submit(function(e) {
+    e.preventDefault();
+    moveTo('cluster');
 });
