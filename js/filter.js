d907ed55ccc2b079d85491424826c2fdf7c3a7a9
Assaf Akrabi <akrabi@mail.com>
2015-01-14 23:56:34 +0200

    Add ajax get request based on the shape drawn in filter

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,35 @@
 $(document).ready(function () {
     // Initialize slider
-    var slider = new TimeSlider($('#slider')[0]);
+    var slider = new TimeSlider($('#filterTimeSlider')[0]);
     slider.init()
+    var map = new Map($('#filterLocationMap')[0]);
+    map.init();
+    map.addSearchBox();
+    map.addDrawingManager();
+
+    $('#filterForm').submit(function(e) {
+        e.preventDefault();
+        var shape = map.getShape();
+        if (shape.type === 'circle') {
+            var circle = shape.overlay;
+            var x = circle.getCenter().lat();
+            var y = circle.getCenter().lng();
+            var r = circle.getRadius();
+            $.getJSON('rest/messages/filter/location/circle?x='+x+'&y='+y+'&r='+r, function( data ) {
+                filteredMessages = data;
+                moveTo('cluster');
+            });
+        }
+        else if (shape.type === 'rectangle') {
+            var rectangle = shape.overlay;
+            var x = circle.getCenter().lat();
+            var y = circle.getCenter().lng();
+            var r = circle.getRadius();
+            $.getJSON('rest/messages/filter/location/circle?x='+x+'&y='+y+'&r='+r, function( data ) {
+                filteredMessages = data;
+                moveTo('cluster');
+            });
+        }
+    });
+
 });
