5b3f0d34cfb88ad3792377a04d0fc0859d857665
Omer Arad <snaxoa@gmail.com>
2015-01-17 19:42:29 +0200

    Added the ConvexHull library in order to draw polygons after clustering!

--- js/results.js
+++ js/results.js
@@ -1,10 +1,67 @@
 $(document).ready(function () {
     var map = new Map($('#resultsMap')[0]);
-    map.init(40.71660077, -73.95056784); //TODO optimize zoom and location to display results
+    map.init(40.71660077, -73.95056784);                //TODO optimize zoom and location to display results
     map.hide();
     $('#resultsShowMap').click(function() {
+        loadPolygons(map);
         map.show();
     });
 });
 
+//clusters
+
+function loadPolygons(map) {
+
+//Create a new instance.
+    var convexHull = new ConvexHullGrahamScan();
+    var poly;
+    var polyHull;
+
+clusters.forEach(function (cluster) {
+    poly = new google.maps.Polygon({
+        paths: cluster.map(function (item) {
+            return new google.maps.LatLng(item.lat, item.lon);
+        }),
+        strokeColor: '#000',
+        strokeOpacity: 0.2,
+        strokeWeight: 2,
+        fillColor: '#000',
+        fillOpacity: 0.1
+    });
+
+
+    cluster.forEach(function (message) {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(message.location.latitude, message.location.longitude),
+            map: map
+        });
+        convexHull.addPoint(message.lon, message.lat);
+    });
+
+
+    if (convexHull.points.length > 0) {
+        var hullPoints = convexHull.getHull();
+
+
+        //Convert to google latlng objects
+        hullPoints = hullPoints.map(function (item) {
+            return new google.maps.LatLng(item.y, item.x);
+        });
+
+        console.log(hullPoints);
+
+        polyHull = new google.maps.Polygon({
+            paths: hullPoints,
+            strokeColor: '#000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#000',
+            fillOpacity: 0.35
+        });
+
+        polyHull.setMap(map);
+
+    }
+     });
+}
 
